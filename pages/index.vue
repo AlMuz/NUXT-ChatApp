3dd498444598c2935d62eef7d700210e22d6e4fe
AlMuz <almuz@inbox.lv>
2020-05-08 16:44:21 +0300

    Configuration of socketio on FE side

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6> </v-flex>
+    <v-flex xs12 sm8 md6>
+      <v-btn @click="message">
+        New Message
+      </v-btn>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
-export default {}
+export default {
+  sockets: {
+    connect: function() {
+      console.log('socket connected')
+    },
+    customEmit: function(data) {
+      console.log(
+        'this method was fired by the socket server. eg: io.emit("customEmit", data)'
+      )
+    }
+  },
+  methods: {
+    message() {
+      this.$socket.emit('createMessage', {
+        text: 'from client'
+      })
+    }
+  }
+}
 </script>
--- server/app.js
+++ server/app.js
@@ -3,7 +3,17 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 io.on('connection', (socket) => {
-  console.log('connected')
+  socket.on('createMessage', (data) => {
+    setTimeout(() => {
+      socket.emit(
+        'newMessage',
+        {
+          text: `${data.text} server`
+        },
+        1000
+      )
+    })
+  })
 })
 
 module.exports = {
