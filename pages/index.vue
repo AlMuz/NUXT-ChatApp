d56fa03550efb3c3316ed151cb114854ab04ec00
AlMuz <almuz@inbox.lv>
2020-05-09 16:44:33 +0300

    Login page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,45 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-btn @click="message">
-        New Message
-      </v-btn>
+    <v-flex xs12 sm8>
+      <v-card>
+        <v-card-title>
+          Nuxt Chat
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="name"
+              :counter="16"
+              :rules="nameRules"
+              label="Name"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Enter room"
+              required
+            ></v-text-field>
+
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4"
+              @click="submit"
+            >
+              Submit
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 export default {
+  layout: 'empty',
   sockets: {
     connect: function() {
       console.log('socket connected')
@@ -20,11 +50,21 @@ export default {
       )
     }
   },
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      (v) => !!v || 'Name is required',
+      (v) => (v && v.length <= 16) || 'Name must be less than 16 characters'
+    ],
+    room: '',
+    roomRules: [(v) => !!v || 'Room is required']
+  }),
+
   methods: {
-    message() {
-      this.$socket.emit('createMessage', {
-        text: 'from client'
-      })
+    submit() {
+      if (this.$refs.form.validate()) {
+      }
     }
   }
 }
