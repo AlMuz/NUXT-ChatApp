3881440dfcd4ad3dce471688a6dcccdef75185ef
AlMuz <almuz@inbox.lv>
2020-05-16 15:38:16 +0300

    Notifcations realized

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -5,9 +5,35 @@
         <nuxt />
       </v-container>
     </v-content>
+    <v-snackbar v-model="snackbar" bottom color="info">
+      {{ snackbarText }}
+      <v-btn color="red" text @click="snackbar = false">
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      snackbar: false,
+      snackbarText: ''
+    }
+  },
+  mounted() {
+    // messages with their text
+    const messages = []
+    messages['noUser'] = 'User not authorized.'
+    messages['leftChat'] = 'You successfully left the chat.'
+
+    // if message query is in url
+    if (this.$route.query.message) {
+      // show snackbar with message
+      this.snackbarText = messages[this.$route.query.message]
+      this.snackbar = true
+    }
+  }
+}
 </script>
