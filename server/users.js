39769d43cf40dd657d61f0cae1baa4b8d52f0c60
AlMuz <almuz@inbox.lv>
2020-05-16 15:19:50 +0300

    More comments

--- server/users.js
+++ server/users.js
@@ -1,16 +1,20 @@
+// users storage
 class Users {
   constructor() {
     this.users = []
   }
 
+  // just pushing to users array new user
   add(user) {
     this.users.push(user)
   }
 
+  // finding user from users array
   get(id) {
     return this.users.find((user) => user.id === id)
   }
 
+  // removing user from array and return his data
   remove(id) {
     const user = this.get(id)
 
@@ -21,6 +25,7 @@ class Users {
     return user
   }
 
+  // getting users by room name
   getByRoom(room) {
     return this.users.filter((user) => user.room === room)
   }
